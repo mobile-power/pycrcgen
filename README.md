b96040a89485ebca47804eef5b037bf089984143
Joshua O'Leary <josh.oleary@mobile-power.co.uk>
2020-06-11 10:58:46 +0100

    add setup.py file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pycrcgen
 Python CRC generator, fork of the now removed https://pycrc.readthedocs.io
+
+Original authors Cristian Năvălici (cristian.navalici@runbox.com) and Joshua Pereyda (joshua.t.pereyda@gmail.com)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='pycrcgen',
+    version='1.3',
+    description='fork of pycrc',
+    author='Joshua O\'Leary',
+    author_email='josh.oleary@mobile-power.co.uk',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['PyCRC'],
+)
